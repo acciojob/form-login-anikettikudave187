90a116d3bb8459700568971c2c0606e545208df0
Aniket Tikudave <anikettikudave187@gmail.com>
2025-06-20 13:20:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
     //Write your code here
+	event.preventDefault();
+
+	let form=document.getElementById("form1");
+	let name=form.fname.value;
+	let lastName=form.lname.value;
+	let fullName=name+" "+lastName;
+	alert(fullName);
 
 }
